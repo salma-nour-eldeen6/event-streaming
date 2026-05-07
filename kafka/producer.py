60b957daf777bd0be3caf94ef4287d8a49b3a41d
Mariam Eid <mariameid210@gmail.com>
2026-05-07 18:31:32 +0300

    fix: use kafka internal listener for containerized producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 # Kafka setup
 try:
     producer = KafkaProducer(
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers='kafka:29092',
         key_serializer=lambda k: k.encode('utf-8'),
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
