468ba6c58547b857c32a4bc19b147d6769623753
Mariam Eid <mariameid210@gmail.com>
2026-05-07 17:00:36 +0300

    feat: orchestrate Kafka producer and Flink jobs with Airflow

--- dags/atlas_test_jobs_dag.py
+++ dags/atlas_test_jobs_dag.py
@@ -19,11 +19,15 @@ with DAG(
 
     test_docker_access = BashOperator(
         task_id="test_docker_access",
-        bash_command="docker ps --format '{{{{.Names}}}}'",
+        bash_command="docker ps",
     )
     create_topic = BashOperator(
     task_id="create_kafka_topic",
     bash_command="bash /opt/airflow/scripts/create_kafka_topic.sh",
+    )
+    run_producer = BashOperator(
+    task_id="run_kafka_producer",
+    bash_command="bash /opt/airflow/scripts/run_kafka_producer.sh",
     )
 
     run_bronze = BashOperator(
@@ -41,5 +45,9 @@ with DAG(
         bash_command="bash /opt/airflow/scripts/run_gold_job.sh",
     )
 
-    # test_scripts_mount >> test_docker_access
-    # test_docker_access >> [run_bronze, run_silver, run_gold]
+    test_scripts_mount >> test_docker_access
+    test_docker_access >> create_topic
+    create_topic >> run_bronze
+    run_bronze >> run_silver
+    run_silver >> run_producer
+    run_producer >> run_gold
